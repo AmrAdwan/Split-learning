ad729e86b870215fa5a4e90c854af7ff96b56a62
A. Adwan <a.adwan@umail.leidenuniv.nl>
2023-02-14 20:43:23 +0100

    run python script multiple times

---
+++ split-learning/run.py
@@ -0,0 +1,11 @@
+import os
+
+# os.sys("Split-NN-Benchmark.py")
+
+# command = 'python Split-NN-Benchmark.py'
+# exec(command)
+
+for i in range(3):
+    # os.sys("Split-NN-Benchmark.py.py")
+    exec(open("Split-NN-Benchmark.py").read())
+    # execfile("Split-NN-Benchmark.py")
