fa866b6ce999dfcb25fc87452f1fd628cf5e4812
A. Adwan <a.adwan@umail.leidenuniv.nl>
2023-02-17 08:49:08 +0100

    run python script multiple times

--- split-learning/run.sh
+++ split-learning/run.sh
@@ -1,6 +1,6 @@
 
 # This command for local experiments 
-for i in `seq 1 20`; do python3.7 Split-NN-Benchmark.py; done 
+# for i in `seq 1 20`; do python3.7 Split-NN-Benchmark.py; done 
 
 # This command for uni computer experiments
 for i in `seq 1 20`; do python Split-NN-Benchmark.py; done 
